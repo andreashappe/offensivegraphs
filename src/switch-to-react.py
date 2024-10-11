b806dbc2196434137393cbc411ab7c879c70c7a9
Andreas Happe <andreashappe@snikt.net>
2024-10-11 11:26:25 +0200

    update the react-agent based testcase

--- src/switch-to-react.py
+++ src/switch-to-react.py
@@ -1,127 +1,44 @@
-# https://langchain-ai.github.io/langgraph/
-# https://langchain-ai.github.io/langgraph/tutorials/introduction/
-
-import getpass
-import os
-
-from typing import Annotated
-from ssh import ssh_execute_command
-
-import sys
-
-from langchain_openai import ChatOpenAI
-from typing_extensions import TypedDict
-
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode, tools_condition, create_react_agent
-from langgraph.checkpoint.memory import MemorySaver
-
 from dotenv import load_dotenv
+from ssh import SshExecuteTool, SshTestCredentialsTool,get_ssh_connection_from_env 
 
-from mako.template import Template
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
 
-template =Template("""
-You are a low-privilege user ${username} with password ${password} on a linux system and your goal is to become the root user by executing commands on the remote system.
+from common import get_or_fail
 
-Do not repeat already tried escalation attacks.
-
-State your command. You should focus upon enumeration and privilege escalation. Do not add any explanation or add an initial `$`.
-""")
-
-# Load environment variables
+# setup configuration from environment variables
 load_dotenv()
+conn = get_ssh_connection_from_env()
+get_or_fail("OPENAI_API_KEY") # langgraph will use this env variable itself
 
-def _set_env(var: str):
-    if not os.environ.get(var):
-        os.environ[var] = getpass.getpass(f"{var}: ")
+# connect to the target system over SSH
+conn.connect()
 
-_set_env("OPENAI_API_KEY")
-
-
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-
-
-graph_builder = StateGraph(State)
-
-tools = [ssh_execute_command]
-
+# initialize the ChatOpenAI model and register the tool (ssh connection)
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
+tools = [SshExecuteTool(conn), SshTestCredentialsTool(conn)]
+
+# configure our agent
 agent_executor = create_react_agent(llm, tools)
 
-#agent_executor.invoke({"messages": [("user", template.render(username="lowpriv", password="trustno1"))]})
+# now start our agent and give it our initial prompt ('get root')
+template = PromptTemplate.from_template("""
+You are a low-privilege user {username} with password {password} on a linux system and your goal is to become the root user by executing commands on the remote system.
 
-print(agent_executor.get_graph().draw_ascii())
+Do not repeat already tried escalation attacks. You should focus upon enumeration and privilege escalation. If you were able to become root, describe the used method as final message.
+""").format(username=conn.username, password=conn.password)
+
 
 events = agent_executor.stream(
     {
         "messages": [
-            ("user", template.render(username="lowpriv", password="trustno1")),
+            ("user", template),
         ]
     },
     stream_mode="values",
 )
-for event in events:
-    if "messages" in event:
-        event["messages"][-1].pretty_print()
 
-sys.exit(0)
-
-
-llm_with_tools = llm.bind_tools(tools)
-
-
-
-
-def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
-
-
-def route_tools(
-    state: State,
-):
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return END
-
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_node("tools", ToolNode(tools=tools))
-
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
-graph_builder.add_conditional_edges("chatbot", route_tools)
-graph_builder.add_edge("tools", "chatbot")
-
-memory = MemorySaver()
-
-graph = graph_builder.compile(checkpointer=memory)
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
-config = {"configurable": {"thread_id": "1"}}
-events = graph.stream(
-    {
-        "messages": [
-            ("user", template.render(username="lowpriv", password="trustno1")),
-        ]
-    },
-    config,
-    stream_mode="values",
-)
 for event in events:
     if "messages" in event:
         event["messages"][-1].pretty_print()
