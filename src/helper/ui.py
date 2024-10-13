b1441fdb92895a0825c50784e778c5370e539b0c
Andreas Happe <andreashappe@snikt.net>
2024-10-13 15:40:39 +0200

    use the graph/initial_version within plan_and_execute.py

--- src/helper/ui.py
+++ src/helper/ui.py
@@ -4,26 +4,29 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.pretty import Pretty
 
-def print_event_stream(console: Console, events):
-    for event in events:
-        if "messages" in event:
-            message = event["messages"][-1]
-            if isinstance(message, HumanMessage):
-                console.print(Panel(str(message.content), title="Punny Human says"))
-            elif isinstance(message, ToolMessage):
-                console.print(Panel(str(message.content), title=f"Tool Reponse from {message.name}"))
-            elif isinstance(message, AIMessage):
-                if message.content != '':
-                    console.print(Panel(str(message.content), title="AI says"))
-                elif len(message.tool_calls) == 1:
-                    tool = message.tool_calls[0]
-                    console.print(Panel(Pretty(tool["args"]), title=f"Tool Call to {tool["name"]}"))
-                else:
-                    print("WHAT do you want?")
-                    console.log(message)
+def print_event(console: Console, event):
+    if "messages" in event:
+        message = event["messages"][-1]
+        if isinstance(message, HumanMessage):
+            console.print(Panel(str(message.content), title="Punny Human says"))
+        elif isinstance(message, ToolMessage):
+            console.print(Panel(str(message.content), title=f"Tool Reponse from {message.name}"))
+        elif isinstance(message, AIMessage):
+            if message.content != '':
+                console.print(Panel(str(message.content), title="AI says"))
+            elif len(message.tool_calls) == 1:
+                tool = message.tool_calls[0]
+                console.print(Panel(Pretty(tool["args"]), title=f"Tool Call to {tool["name"]}"))
             else:
-                print("WHAT message are you?")
+                print("WHAT do you want?")
                 console.log(message)
         else:
-            print("WHAT ARE YOU??????")
-            console.log(event)
+            print("WHAT message are you?")
+            console.log(message)
+    else:
+        print("WHAT ARE YOU??????")
+        console.log(event)
+
+def print_event_stream(console: Console, events):
+    for event in events:
+        print_event(console, event)
--- src/plan_and_execute.py
+++ src/plan_and_execute.py
@@ -3,21 +3,23 @@ import time
 
 from dotenv import load_dotenv
 from graphs.plan_and_execute import PlanExecute
+from rich.console import Console
 
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from helper.common import get_or_fail
-from tools.ssh import get_ssh_connection_from_env 
+from helper.ui import print_event
+from tools.ssh import get_ssh_connection_from_env, SshTestCredentialsTool, SshExecuteTool
 from graphs.initial_version import create_chat_tool_agent_graph
 
 from graphs.plan_and_execute import create_plan_and_execute_graph
-from initial_version import graph as executor_graph
 
 # setup configuration from environment variables
 load_dotenv()
 get_or_fail("OPENAI_API_KEY") # langgraph will use this env variable itself
 conn = get_ssh_connection_from_env()
+conn.connect()
 
 # now start our agent and give it our initial prompt ('get root')
 template = PromptTemplate.from_template("""
@@ -29,13 +31,23 @@ Do not repeat already tried escalation attacks. You should focus upon enumeratio
 # initialize the ChatOpenAI model and register the tool (ssh connection)
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
+# configure a second LLM connection for the exeuctor_step
+llm2 = ChatOpenAI(model="gpt-4o", temperature=0)
+tools = [SshExecuteTool(conn), SshTestCredentialsTool(conn)]
+llm2_with_tools = llm2.bind_tools(tools)
+
 async def execute_step(state: PlanExecute):
     plan = state["plan"]
     plan_str = "\n".join(f"{i+1}. {step}" for i, step in enumerate(plan))
     task = plan[0]
     task_formatted = f"""For the following plan:
 {plan_str}\n\nYou are tasked with executing step {1}, {task}. Stop after 10 command executions."""
-    agent_response = await executor_graph.ainvoke(
+    
+    # create our simple graph
+    graph_builder = create_chat_tool_agent_graph(llm2_with_tools, tools)
+    graph = graph_builder.compile()
+
+    agent_response = await graph.ainvoke(
         {"messages": [("user", task_formatted)]},
         config = {
             "configurable": {"thread_id": f"thread-{time.time()}" }
@@ -45,6 +57,9 @@ async def execute_step(state: PlanExecute):
     return {
         "past_steps": [(task, agent_response["messages"][-1].content)],
     }
+
+# prepare console
+console = Console()
 
 # create the graph
 workflow = create_plan_and_execute_graph(llm, execute_step)
@@ -57,16 +72,6 @@ inputs = {"input": template }
 # start everything
 async def start():
     async for event in app.astream(inputs, config=config):
-        if "messages" in event:
-            print("!!!! message:")
-            event["messages"][-1].pretty_print()
-        elif "plan" in event:
-            print("!!!! plan:")
-            event["plan"][-1].pretty_print()
-        else:
-            print("Not event!")
-            for k, v in event.items():
-                if k != "__end__":
-                    print(v)
+        print_event(console, event)
 
 asyncio.run(start())
