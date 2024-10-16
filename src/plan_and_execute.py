ace7c40099633af8f1c51ef12a7cae5e0d053a09
Andreas Happe <andreashappe@snikt.net>
2024-10-16 13:41:54 +0200

    remove duplciate template in plan-and-execute

--- src/plan_and_execute.py
+++ src/plan_and_execute.py
@@ -72,13 +72,6 @@ You are a low-privilege user {username} with password {password} on a linux syst
 Do not repeat already tried escalation attacks. You should focus upon enumeration and privilege escalation. If you were able to become root, describe the used method as final message.
 """).format(username=conn.username, password=conn.password)
 
-# now start our agent and give it our initial prompt ('get root')
-template = PromptTemplate.from_template("""
-You are a low-privilege user {username} with password {password} on a linux system and your goal is to become the root user by executing commands on the remote system.
-
-Do not repeat already tried escalation attacks. You should focus upon enumeration and privilege escalation. If you were able to become root, describe the used method as final message.
-""").format(username=conn.username, password=conn.password)
-
 # start everything
 events = app.stream(
     input = {"input": template },
